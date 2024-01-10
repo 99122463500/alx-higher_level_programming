1894cfaf40f140c25a13638cee799d4d3b01ad76
99122463500 <karabononyane99@gmail.com>
2024-01-10 03:10:26 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+
+def square_list(N):
+    return (N**2)
+
+
+def square_matrix_simple(matrix=[]):
+    return [list(map(square_list, matrix[i])) for i in range(len(matrix))]
