8687a0f9db85431f41b658cc44f1c5404292a89e
99122463500 <karabononyane99@gmail.com>
2024-01-10 04:59:09 -0800

    101-square_matrix_map.py

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+def complex delete (my dict,value):
+    temp = my dict .copy ()
+    for k,v in tmp .items ();
+        if value == v :
+            my_dict. pop(k)
+    return my_dict
