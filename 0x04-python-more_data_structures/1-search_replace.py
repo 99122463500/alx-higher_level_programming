ac48cfaf1245d447872a81b60e2ac6de29b0636b
99122463500 <karabononyane99@gmail.com>
2024-01-10 03:13:11 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
-        return 'None'
-    else:
-        return my_list[idx]
+def search_replace(my_list, search, replace):
+    return [replace if i == search else i for i in my_list]
