888452018648fb6a5d5cb9c29ebe8e12a4c82a59
99122463500 <karabononyane99@gmail.com>
2024-02-04 21:03:47 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/tests/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A module to create an empty class
+named BaseGeometry """
+
+
+class BaseGeometry:
+    """ An empty class """
+    def area(self):
+        """ a Function to print the area
+
+        Args:
+            self: a given default parameter
+        """
+        raise Exception("area() is not implemented")
