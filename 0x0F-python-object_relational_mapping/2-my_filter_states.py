867d8b7c3faae8dc4a0b2ee70e5af252b7f7ee0a
99122463500 <karabononyane99@gmail.com>
2024-04-12 06:57:44 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""A Script that will lists all states from the database hbtn_0e_0_usa
+But with the name is mathcing the argument passed! :)
+"""
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+
+    HOST = "localhost"
+    PORT = 3306
+    USER = argv[1]
+    PASS = argv[2]
+    DB = argv[3]
+    state_name = argv[4]
+    db = MySQLdb.connect(host=HOST,
+                         port=PORT,
+                         user=USER,
+                         passwd=PASS,
+                         db=DB,
+                         charset="utf8")
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = BINARY '{}' ORDER BY states.id"
+    cursor.execute(query.format(state_name))
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
