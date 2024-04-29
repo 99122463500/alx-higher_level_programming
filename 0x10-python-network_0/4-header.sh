c2c9da2fcb914d729168c434e1c471dd6c50434a
99122463500 <karabononyane99@gmail.com>
2024-04-29 10:43:30 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# using delete to pass the url as a first argument
+curl -s "$1" -X DELETE
