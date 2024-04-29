55efaf3a87d14e0b1408ab306399bd622f6ae47f
99122463500 <karabononyane99@gmail.com>
2024-04-29 10:35:13 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Sends a GET request to a given URL and display the response status code.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+#!/usr/bash
+# a script that sends a request to a URL passed as an argument then shows the status code
+curl -s -o /dev/null -w '%{http_code}' "$1"
